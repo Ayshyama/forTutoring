8d4c06b6252322dc573cf4d779d8321cd6931cd2
Ayshyama@gmail.com <Ayshyama@gmail.com>
2023-10-19 14:28:40 +0200

    update

--- 5_Django_study_project/app_users/urls.py
+++ 5_Django_study_project/app_users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import register, user_login, user_logout
+from .views import register, user_login, user_logout, ProfileDetailView
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
-
+    path('profile/<str:username>/', ProfileDetailView.as_view(), name='profile-details'),  # Use <str:username>
 ]
 
 
